e867b8f2c7554f4fa2c0dcce8daed597cf5e9240
Prajanya Shrestha <Prajanya.stha7@gmail.com>
2022-05-13 20:17:48 +0545

    Paginated

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes();
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/', [App\Http\Controllers\PostsController::class, 'index']);
 
 Route::post('/follow/{user}', [App\Http\Controllers\FollowsController::class, 'store']);
 
